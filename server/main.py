24b63e98d8f26c1345b3a98dc31cbee708276c40
Michail Zotin <apxahrejl@gmail.com>
2018-12-03 08:54:09 +0500

    Add config to main (temporary)

--- server/main.py
+++ server/main.py
@@ -6,7 +6,9 @@ from flask import Flask, request, render_template, jsonify, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 
 
-import config.config as config
+config = {
+    'mode': 'release'
+}
 
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
